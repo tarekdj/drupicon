6711693fdd6daaf29eaef6b75978d6f57de61518
Tarek Djebali <tarekdj@gmail.com>
2020-05-12 16:58:34 +0200

    Initial commit

---
+++ src/bg/background.js
@@ -0,0 +1,7 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+    if (changeInfo.status === 'complete') {
+        chrome.tabs.executeScript(tabId, {
+            code: 'document.querySelector(\'link[rel*="icon"]\').href = "//placehold.it/32/F00"'
+        });
+    }
+});

1676b8fef499de4529d5f4c1f85f147159ea68da
Tarek Djebali <tarekdj@gmail.com>
2020-05-12 17:17:59 +0200

    Restore _locales folder and update favicon url

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
     if (changeInfo.status === 'complete') {
         chrome.tabs.executeScript(tabId, {
-            code: 'document.querySelector(\'link[rel*="icon"]\').href = "//"'
+            code: 'document.querySelector(\'link[rel*="icon"]\').href = "//raw.githubusercontent.com/tarekdj/drupicon/master/icons/icon128.png"'
         });
     }
 });
